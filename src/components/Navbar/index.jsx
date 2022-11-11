8e0978a431dc99ea74f69388dab22ebae2689230
tharuAnish <namoanishtharu@gmail.com>
2022-11-11 17:45:52 +0530

    navbar created

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,10 @@
+import './navbar.style.css'
+
+export default function Navbar (){
+    return(
+        <div className="navbar">
+            <img className='nav-logo' src="./troll-face.png" alt="image" />
+            <h2 className='nav-heading'>Meme Generator</h2>
+        </div>
+    )
+}
